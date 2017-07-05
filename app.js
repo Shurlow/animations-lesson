ccf074c61f69184d180cb9c8a02c6ba98d4cfe36
shurlow <scottyhurlow@gmail.com>
2017-07-05 14:43:40 -0700

    general cleanup

--- app.js
+++ app.js
@@ -22,30 +22,3 @@ var cube = anime({
 document.querySelector('.anime2').addEventListener('click', function(e) {
   cube.restart()
 })
-
-
-// document.querySelector('#shake').addEventListener('click', function(e) {
-//   this.classList.toggle('animated')
-//   this.classList.toggle('shake')
-//   // $(this).addClass('shake')
-// })
-//
-// document.querySelector('#shake').addEventListener('click', function(e) {
-//   e.target.removeEventListener(e.type, arguments.callee);
-//   console.log('SEE ME ONCE!', arguments)
-// })
-
-// 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', doSomething
-
-
-// var shake = anime({
-//   targets: '.shake',
-//   left: '240px',
-//   color: '#ccc',
-//   autoplay: false
-// })
-//
-//
-// document.querySelector('.shake').addEventListener('click', function(e) {
-//   shake.play()
-// })
